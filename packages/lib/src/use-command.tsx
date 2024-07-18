19107e6f24f49c8aeb13b0cf2eb50998046aeef7
afeiship <1290657123@qq.com>
2024-07-18 23:22:12 +0800

    fix: name should be null or a string

--- packages/lib/src/use-command.tsx
+++ packages/lib/src/use-command.tsx
@@ -1,21 +1,20 @@
 import ReactInteractiveList from '.';
 import { useEffect, useState } from 'react';
 
-const useCommand = (name: string) => {
-  const [value, setValue] = useState<any>();
-  const add = () => value.emit(`${name}:add`);
-  const remove = (index: number) => value.emit(`${name}:remove`, index);
-  const set = (items: any[]) => value.emit(`${name}:set`, items);
-  const up = (index: number) => value.emit(`${name}:up`, index);
-  const down = (index: number) => value.emit(`${name}:down`, index);
-  const clear = () => value.emit(`${name}:clear`);
-  const notify = () => value.emit(`${name}:notify`);
+const useCommand = (inName?: string) => {
+  const name = inName || '@';
+  const [emitter, setEmitter] = useState<any>();
+  const add = () => emitter.emit(`${name}:add`);
+  const remove = (index: number) => emitter.emit(`${name}:remove`, index);
+  const set = (items: any[]) => emitter.emit(`${name}:set`, items);
+  const up = (index: number) => emitter.emit(`${name}:up`, index);
+  const down = (index: number) => emitter.emit(`${name}:down`, index);
+  const clear = () => emitter.emit(`${name}:clear`);
+  const notify = () => emitter.emit(`${name}:notify`);
 
   useEffect(() => {
-    setValue(ReactInteractiveList.event);
+    setEmitter(ReactInteractiveList.event);
   }, []);
-
-  console.log('value: ', value);
 
   return {
     add,
